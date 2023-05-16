82fd9809c14ffa1c858574f8f6b75bba72e9ca84
lvads8 <lvads8@inf.elte.hu>
2023-05-16 19:07:10 +0000

    add tailwind

---
+++ resources/views/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>IoT 2023</title>
+
+    @vite('resources/css/app.css')
+</head>
+<body class="antialiased">
+    <h1 class="text-3xl font-bold underline">Hello, world!</h1>
+</body>
+</html>
