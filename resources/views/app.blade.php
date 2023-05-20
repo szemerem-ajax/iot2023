c786f94b110e9a00ae492a9eedc92256eb3ee433
lvads8 <lvads8@inf.elte.hu>
2023-05-20 15:05:15 +0000

    finalise ui

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,4 +1,5 @@
 @extends('layout')
 @section('content')
 <h1 class="text-3xl font-bold text-gray-50">Welcome to the dashboard!</h1>
+<a href="{{ route('uzems.index') }}" class="text-amber-600 transition-colors underline hover:text-amber-500">Go to plants</a>
 @endsection
